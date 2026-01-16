8dd6ed2125c482ffc89738ee049fb0a1a4491303
ColeW96 <135039107+ColeW96@users.noreply.github.com>
2026-01-15 20:47:11 -0500

    Project Updates

--- 00_global/save_manager.gd
+++ 00_global/save_manager.gd
@@ -1,6 +1,8 @@
 # SaveManager
 extends Node
 
+const CONFIG_FILE_PATH = "user://settings.cfg"
+
 # Forest level 01
 const DEFAULT_SCENE_PATH : String = "uid://cuhy61b0drxbf"
 const SLOTS : Array[ String ] = [
@@ -16,6 +18,7 @@ var persistent_data : Dictionary = {}
 
 
 func _ready() -> void:
+	load_configuration()
 	SceneManager.scene_entered.connect( _on_scene_entered )
 	pass
 
@@ -144,3 +147,32 @@ func _on_scene_entered( scene_uid : String ) -> void:
 	else:
 		discovered_areas.append( scene_uid )
 	pass
+
+
+#region /// Configuration functions
+
+func save_configuration() -> void:
+	var config := ConfigFile.new()
+	config.set_value( "audio", "music", AudioServer.get_bus_volume_linear( 2 ) )
+	config.set_value( "audio", "sfx", AudioServer.get_bus_volume_linear( 3 ) )
+	config.set_value( "audio", "ui", AudioServer.get_bus_volume_linear( 4 ) )
+	config.save( CONFIG_FILE_PATH )
+	pass
+
+func load_configuration() -> void:
+	var config := ConfigFile.new()
+	var err = config.load( CONFIG_FILE_PATH )
+	
+	if err != OK:
+		AudioServer.set_bus_volume_linear( 2, 0.8 )
+		AudioServer.set_bus_volume_linear( 3, 1.0 )
+		AudioServer.set_bus_volume_linear( 4, 1.0 )
+		save_configuration()
+		return
+	
+	AudioServer.set_bus_volume_linear( 2, config.get_value( "audio", "music", 0.8 ) )
+	AudioServer.set_bus_volume_linear( 3, config.get_value( "audio", "sfx", 1.0 ) )
+	AudioServer.set_bus_volume_linear( 4, config.get_value( "audio", "ui", 1.0 ) )
+	pass
+	
+#endregion
