5f34a16c504d77f7998d6286a3a35ce499a56496
ColeW96 <135039107+ColeW96@users.noreply.github.com>
2026-01-31 11:16:02 -0500

    Add Debug build checks
    adds checks for project export debug code

--- 00_global/save_manager.gd
+++ 00_global/save_manager.gd
@@ -24,17 +24,20 @@ func _ready() -> void:
 
 
 func _unhandled_key_input( event: InputEvent ) -> void:
-	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_F5:
-			save_game()
-		elif event.keycode == KEY_F7:
-			load_game( current_slot )
-		elif event.keycode == KEY_1:
-			current_slot = 0
-		elif event.keycode == KEY_2:
-			current_slot = 1
-		elif event.keycode == KEY_3:
-			current_slot = 2
+	# DEBUG
+	if OS.is_debug_build():
+		if event is InputEventKey and event.pressed:
+			if event.keycode == KEY_F5:
+				save_game()
+			elif event.keycode == KEY_F7:
+				load_game( current_slot )
+			elif event.keycode == KEY_1:
+				current_slot = 0
+			elif event.keycode == KEY_2:
+				current_slot = 1
+			elif event.keycode == KEY_3:
+				current_slot = 2
+	# end DEBUG
 	pass
 
 
