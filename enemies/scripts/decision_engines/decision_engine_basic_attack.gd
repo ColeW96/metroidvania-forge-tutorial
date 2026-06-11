b6c60f3982af1d2be368c58d23665e3fae87bf0f
ColeW96 <cole.whitaker18@gmail.com>
2026-06-11 12:23:09 -0400

    Update living armor and add basic enemy state classes

--- enemies/scripts/decision_engines/decision_engine_basic_attack.gd
+++ enemies/scripts/decision_engines/decision_engine_basic_attack.gd
@@ -6,6 +6,14 @@ extends DecisionEngine
 # var current_state : EnemyState
 # var blackboard : Blackboard
 
+@export var attack_state : ESAttack
+@export var chase_state : EnemyState
+
+@onready var es_walk: ESWalk = %ESWalk
+@onready var es_stun: ESStun = %ESStun
+@onready var es_death: ESDeath = %ESDeath
+
+
 
 func _ready() -> void:
 	await super()
@@ -15,17 +23,17 @@ func _ready() -> void:
 # All the conditions for making decisions go in this function
 func decide() -> EnemyState:
 	# Example decisions
-	#if blackboard.damage_source:
-		#if blackboard.health <= 0:
-			#return es_death
-		#else:
-			#return es_stun
+	if blackboard.damage_source:
+		if blackboard.health <= 0:
+			return es_death
+		else:
+			return es_stun
 	
 	if current_state is ESDeath or not blackboard.can_decide:
 		return null
 	
-	#if blackboard.target:
-		#if blackboard.distance_to_target < 40:
-			#return attack_state?
-		#return change_state?
-	return null # default state
+	if blackboard.target:
+		if attack_state.can_attack():
+			return attack_state
+		return chase_state
+	return es_walk # default state
