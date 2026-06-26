296573bd9407c44987dc57ed9a3749b6cd01fc4f
ColeW96 <cole.whitaker18@gmail.com>
2026-06-26 14:37:20 -0400

    Add ledge grabbing

---
+++ player/states/ledge_grab.gd
@@ -0,0 +1,34 @@
+class_name PlayerStateLedgeGrab extends PlayerState
+
+var old_gravity : float = 980.0
+var jump_velocity : float = 450
+
+func enter() -> void:
+	player.grabbing_ledge = true
+	player.animation_player.play("ledge_grab")
+	player.jump_count = 0
+	player.velocity = Vector2.ZERO
+	player.gravity = 0
+	
+	player.ledge_floor_check.force_raycast_update()
+	if player.ledge_floor_check.is_colliding():
+		var ledge_point : Vector2 = player.ledge_floor_check.get_collision_point()
+		var snap_offset : Vector2 = ledge_point - player.ledge_grab_point.global_position
+		var target_pos : Vector2 = player.global_position + snap_offset
+		var tween : Tween = player.create_tween()
+		tween.set_trans(Tween.TRANS_SINE)
+		tween.set_ease(Tween.EASE_OUT)
+		tween.tween_property( player, "global_position", target_pos, 0.05 )
+	pass
+
+func handle_input( event : InputEvent ) -> PlayerState:
+	if event.is_action_pressed("jump"):
+		return jump
+	return null
+
+
+func exit() -> void:
+	player.grabbing_ledge = false
+	player.velocity.y = -jump_velocity
+	player.gravity = old_gravity
+	pass
