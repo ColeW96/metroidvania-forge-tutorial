6fa265be4afb4488e7c7b1f4af36669735fab18a
ColeW96 <cole.whitaker18@gmail.com>
2026-08-11 13:38:28 -0400

    Updates for boss battle orchestration

--- bosses/scripts/boss_battle_orchestrator.gd
+++ bosses/scripts/boss_battle_orchestrator.gd
@@ -8,9 +8,46 @@ signal boss_reward_collected
 @export var boss : Node
 @export var trigger_area : Area2D
 
+@export_category("Boss Music")
+@export var boss_track : AudioStream
+@export var post_boss_track : AudioStream
+
+@export_category("Camera Bounds")
+@export var boss_level_bounds : LevelBounds
+@export var original_level_bounds : LevelBounds
 
 func _ready() -> void:
 	if boss:
 		boss.process_mode = Node.PROCESS_MODE_DISABLED
 	
+	if trigger_area:
+		trigger_area.set_collision_mask_value( 5, true )
+		trigger_area.set_collision_mask_value( 1, false )
+		trigger_area.set_collision_layer_value( 1, false )
+		trigger_area.body_entered.connect( _on_body_entered )
+	pass
+
+
+func start_boss_battle() -> void:
+	battle_started.emit()
+	
+	Audio.play_music( boss_track )
+	if boss:
+		boss.process_mode = Node.PROCESS_MODE_INHERIT
+		boss.tree_exiting.connect( end_boss_battle )
+	pass
+
+
+func end_boss_battle() -> void:
+	battle_ended.emit()
+	print("Boss battle ended")
+	
+	Audio.play_music( post_boss_track )
+	pass
+
+
+func _on_body_entered( body : Node2D ) -> void:
+	if body is Player:
+		start_boss_battle()
+		trigger_area.body_entered.disconnect( _on_body_entered )
 	pass
--- general/scripts/level_bounds.gd
+++ general/scripts/level_bounds.gd
@@ -6,13 +6,19 @@ class_name LevelBounds extends Node2D
 @export_range( 480, 2048, 32, "suffix:px" ) var width: int = 480 : set = _on_width_changed
 @export_range( 270, 2048, 32, "suffix:px" ) var height: int = 270 : set = _on_height_changed
 
+@export var set_on_ready : bool = true : set = _on_bool_changed
+
 
 func _ready() -> void:
 	z_index = 256
 	
-	if Engine.is_editor_hint():
+	if Engine.is_editor_hint() or not set_on_ready:
 		return
 	
+	set_camera_bounds()
+	pass
+
+func set_camera_bounds() -> void:
 	var camera : Camera2D = null
 	
 	while not camera:
@@ -28,9 +34,16 @@ func _ready() -> void:
 
 func _draw() -> void:
 	if Engine.is_editor_hint():
+		var color_01 : Color = Color(0.0, 0.45, 1.0, 0.6)
+		var color_02 : Color = Color(0.0, 0.75, 1.0)
 		var r : Rect2 = Rect2( Vector2.ZERO, Vector2( width, height ) )
-		draw_rect( r, Color( 0.0, 0.45, 1.0, 0.6 ), false, 3 )
-		draw_rect( r, Color( 0.0, 0.75, 1.0 ), false, 1 )
+		
+		if not set_on_ready:
+			color_01 = Color(1.0, 0.302, 0.0, 0.6)
+			color_02 = Color(1.0, 0.395, 0.0, 1.0)
+		
+		draw_rect( r, color_01, false, 3 )
+		draw_rect( r, color_02, false, 1 )
 	pass
 
 
@@ -42,5 +55,11 @@ func _on_width_changed( new_width : int ) -> void:
 
 func _on_height_changed( new_height : int ) -> void:
 	height = new_height
+	queue_redraw()
+	pass
+
+
+func _on_bool_changed( new_value : bool ) -> void:
+	set_on_ready = new_value
 	queue_redraw()
 	pass
