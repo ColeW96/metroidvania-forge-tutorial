3f7d7d59bf9e05c804b91482bf8634e0acb89107
ColeW96 <cole.whitaker18@gmail.com>
2026-08-24 16:00:27 -0400

    Updates for interior levels

--- general/input_level_transition/input_level_transition.gd
+++ general/input_level_transition/input_level_transition.gd
@@ -21,8 +21,7 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("up") and player_in_area:
 		get_viewport().set_input_as_handled()
-		var offset : Vector2 = Vector2(0, 6)
-		SceneManager.transition_scene( target_level, target_area_name, offset, "up" )
+		SceneManager.transition_scene( target_level, target_area_name, Vector2(0, 6), "up" )
 
 
 
